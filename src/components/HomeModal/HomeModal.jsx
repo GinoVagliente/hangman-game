75f1823cf4bf3d215dabaaab9a64ed43c0ffe003
gino <gino1v@hotmail.com>
2025-09-29 22:25:33 -0300

    home changes

--- src/components/HomeModal/HomeModal.jsx
+++ src/components/HomeModal/HomeModal.jsx
@@ -1,7 +1,7 @@
 const HomeModal = () => {
   return (
     <div className="flex justify-center items-center h-full px-4 flex-col">
-      <div className="relative rounded-lg shadow-2xl p-15 flex flex-col items-center gap-4 w-full max-w-xs md:max-w-md backdrop-blur-md">
+      <div className="relative rounded-lg shadow-2xl p-10 flex flex-col items-center gap-4 w-full max-w-xs  backdrop-blur-md">
         <div
           className="absolute inset-0 rounded-lg bg-cover bg-center bg-no-repeat opacity-40 pointer-events-none bg-[url('/homeModalTexture.png')]"
         />
@@ -14,7 +14,7 @@ const HomeModal = () => {
         </p>
         <a
           href="/play"
-          className="text-sm px-4 py-2 md:px-6 md:py-4 text-white rounded-lg hover:bg-gray-100/10 transition-all backdrop-blur-sm text-center relative z-10"
+          className="text-sm px-4 py-2  text-white rounded-lg hover:bg-gray-100/10 transition-all backdrop-blur-sm text-center relative z-10"
           style={{ fontFamily: "'Press Start 2P'" }}
         >
           Start
